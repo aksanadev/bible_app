0fa555abee08b8da6f547889dad23f1aa64d1c9d
aksanadev <aksana.dev@gmail.com>
2023-03-07 12:38:46 -0600

    connected data from service>repository>locator to bloc

--- lib/features/bible/bloc/bible_bloc.dart
+++ lib/features/bible/bloc/bible_bloc.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:bible_app/features/bible/bloc/bible_state.dart';
 import 'package:bible_app/features/bible/bloc/bloc.dart';
 import 'package:bible_app/features/bible/models/bible_model.dart';
+import 'package:bible_app/features/bible/service/api.dart';
+import 'package:bible_app/features/bible/ui/utils/service_locator.dart';
 
 class BibleBloc implements Bloc {
   final _bibleStreamController = StreamController<BibleState>();
@@ -17,6 +19,16 @@ class BibleBloc implements Bloc {
     List<BibleModel> initialBibles = [];
     initialBibles.add(initialBible);
     _bibleState = BibleState(bibles: initialBibles);
+  }
+
+  Future<void> getAllBibles() async {
+    final bibles = await ServiceLocator()
+        .bibleRepository
+        .getAllBibles(method: RestMethod.get, path: '');
+
+    //add bibles to streamController
+    final allBibles = _bibleState.copyWith(bibles: bibles);
+    _bibleStreamController.add(allBibles);
   }
 
   @override
