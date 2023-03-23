81f28b7fcdbbb58a12f38486fd4fb7f17062bcf6
aksanadev <aksana.dev@gmail.com>
2023-03-22 21:17:37 -0500

    cleaned up

--- lib/features/bible/ui/widgets/books_bottom_sheet.dart
+++ lib/features/bible/ui/widgets/books_bottom_sheet.dart
@@ -5,81 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 booksBottomSheet(BuildContext context, BibleState? bibleState, BibleBloc bloc) {
-//   return showModalBottomSheet(
-//     backgroundColor: Colors.transparent,
-//     isScrollControlled: true,
-//     context: context,
-//     builder: (context) {
-//       return Container(
-//         decoration: const BoxDecoration(
-//             color: Colors.white,
-//             borderRadius: BorderRadius.only(
-//                 topLeft: Radius.circular(18), topRight: Radius.circular(18))),
-//         child: FractionallySizedBox(
-//           heightFactor: .93,
-//           child: Column(
-//             children: [
-//               Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                 children: [
-//                   TextButton(
-//                     onPressed: () {
-//                       context.pop();
-//                     },
-//                     child: Text('Done', style: textButton),
-//                   ),
-//                   Column(
-//                     children: [
-//                       Text('Versions', style: defaultStyle),
-//                       Text('2,917 Versions in 1,942 Languages',
-//                           style: smallText)
-//                     ],
-//                   ),
-//                   IconButton(
-//                     onPressed: () {},
-//                     icon: const Icon(Icons.search),
-//                     color: Colors.black,
-//                   )
-//                 ],
-//               ),
-//               Flexible(
-//                 child: SingleChildScrollView(
-//                   physics: const AlwaysScrollableScrollPhysics(
-//                       parent: BouncingScrollPhysics()),
-//                   child: Column(
-//                     children: [
-//                       SizedBox(
-//                         child: Column(
-//                           children: [
-//                             ...bibleState!.bibleBooks!.map(
-//                               (e) => GestureDetector(
-//                                 onTap: () {
-//                                   // bloc.getBibleVersion(e.bibleId);
-//                                   context.pop();
-//                                 },
-//                                 child: ListTile(
-//                                   title: Text(e.bookId.toString()),
-//                                   trailing: IconButton(
-//                                     icon: const Icon(Icons.chevron_left),
-//                                     onPressed: () {},
-//                                   ),
-//                                 ),
-//                               ),
-//                             ),
-//                           ],
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       );
-//     },
-//   );
-// }
   return showModalBottomSheet(
     backgroundColor: Colors.transparent,
     isScrollControlled: true,
@@ -121,11 +46,9 @@ booksBottomSheet(BuildContext context, BibleState? bibleState, BibleBloc bloc) {
                                     bloc.getBibleBook(e.bibleId, e.bookId);
                                   },
                                   child: ListTile(
-                                    title:
-                                        Text(e.bookId.toString()),
+                                    title: Text(e.bookId.toString()),
                                     trailing: IconButton(
-                                      icon:
-                                          const Icon(Icons.chevron_left),
+                                      icon: const Icon(Icons.chevron_left),
                                       onPressed: () {},
                                     ),
                                   )),
